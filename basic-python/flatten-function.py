395984d03e16a9dc0d3e9ff52038af3e113cbd04
alperenyayla <71118638+alperenyayla@users.noreply.github.com>
2022-09-13 13:06:09 +0300

    Update flatten-function.py

--- basic-python/flatten-function.py
+++ basic-python/flatten-function.py
@@ -1,4 +1,3 @@
-
 def flatten(n): # the function makes the lists flatten. n is a list.
     for i in n:
         if isinstance(i, list): # checks the if element of a list is a list, and if it is a list calls the flatten function again.
